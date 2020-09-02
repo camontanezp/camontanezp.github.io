429d6623be9dc0f58f761775ac2ac68d4bf2ccb6
camontanezp <camontanezp@gmail.com>
2020-09-01 20:51:16 -0500

    Reformat

---
+++ plot.js
@@ -0,0 +1,32 @@
+function gaussianRand() {
+    var rand = 0;
+    for (var i = 0; i < 6; i += 1) {
+        rand += Math.random();
+    }
+    return (rand / 6) - 0.5;
+}
+var X = [],
+    Y = [],
+    n = 1000,
+    i;
+
+for (i = 0; i < n; i += 1) {
+    X.push(gaussianRand());
+    Y.push(gaussianRand());
+}
+
+var data = [{
+    type: "scattergl",
+    mode: "markers",
+    marker: {
+        color: '#785964',
+        line: {
+            width: 1,
+            color: '#82A7A6'
+        }
+    },
+    x: X,
+    y: Y
+}]
+
+Plotly.newPlot('plot', data)
